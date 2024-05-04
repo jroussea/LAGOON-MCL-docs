19d1d66001bd48d27f9ba557d7a1cb063a087c95
jroussea <jeremy.rousseau2@outlook.fr>
2024-05-04 16:26:35 +0200

    new version

--- conf.py
+++ conf.py
@@ -1,5 +1,32 @@
 # Configuration file for the Sphinx documentation builder.
 #
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'lagoon-mcl-docs'
+copyright = '2024, Jeremy Rousseau'
+author = 'Jeremy Rousseau'
+release = '0.0.1'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = []
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'alabaster'
+html_static_path = ['_static']# Configuration file for the Sphinx documentation builder.
+#
 # This file only contains a selection of the most common options. For a full
 # list see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
